8ca6994c0f31a70a68b72ab15d847ed04e414ca0
alexdeko <alexdejko@gmail.com>
2018-09-25 01:05:14 +0300

    4 commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+let q = equation
+let a = q.split(" ")[0] , 
+b = q.split(" ")[3] + q.split(" ")[4] , 
+c =q.split(" ")[7] +  q.split(" ")[8] ,
+d = Math.pow(b,2) - 4*a*c ,
+x1  = (-b-Math.sqrt(d))/(2*a) ,
+x2  = (-b+Math.sqrt(d))/(2*a) ,
+s=[Math.round(x1),Math.round(x2)]
+
+return s.sort(function (a, b) {
+    return a - b;
+});
+
 }
